fcc5d04301c3a8c63e775f0beef8ad037f66451c
MD Parves <parvesmd270@gmail.com>
2025-11-10 09:32:28 +0000

    fonts Hind Siliguri

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Poppins', 'sans-serif'],
-        headline: ['Poppins', 'sans-serif'],
+        body: ['"Hind Siliguri"', 'sans-serif'],
+        headline: ['"Hind Siliguri"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
